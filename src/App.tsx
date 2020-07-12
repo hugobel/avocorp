983a65a84d1c76b08d0ecef3cd6af2708a907bb5
hugobel <hola@hugobeltran.com>
2020-07-11 19:18:27 -0500

    Hello world

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World! Demo</p>
+        <p>Hello World! HB</p>
         <a
           className="App-link"
           href="https://reactjs.org"
