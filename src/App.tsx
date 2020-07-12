138e1c3d348f38b54dd07eabb4a5de7bc233e9ec
hugobel <hola@hugobeltran.com>
2020-07-12 13:03:51 -0500

    dev dependencies moved

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +19,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
