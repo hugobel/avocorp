63f0514db37f474565e2166a65a1e5826735542c
hugobel <hola@hugobeltran.com>
2020-07-12 22:37:44 -0500

    Redux goodies

---
+++ src/containers/app/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./app.scss";
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <header className="header">
+        <p className="link">Hello World!</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
