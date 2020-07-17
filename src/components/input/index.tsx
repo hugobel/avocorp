d007c08f574e896dcb87129e9e61b087cd8617db
berthamz <berthamz@gmail.com>
2020-07-16 22:50:33 -0500

    Day 2

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,19 +1,39 @@
 import React from "react";
+import classnames from "classnames";
+import "./input.scss";
 
 type Props = {
-	type?: string;
-	placeholder: string;
-	value: string;
+  type?: string;
+  placeholder: string;
+  value: string;
+  onChange: (v: string) => void;
+  error?: boolean;
 };
 
 const Input: React.FC<Props> = (props) => {
-	const { type, placeholder, value } = props;
+  const { type, placeholder, value, onChange, error } = props;
 
-	return <input type={type} placeholder={placeholder} value={value} />
+  const inputClassNames = classnames("input", {
+    error,
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.value);
+  };
+
+  return (
+    <input
+      className={inputClassNames}
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={handleChange}
+    />
+  );
 };
 
 Input.defaultProps = {
-	type: "text",
-}
+  type: "text",
+};
 
 export default Input;
